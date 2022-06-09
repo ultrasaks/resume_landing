26444f285c24cc54e76595453480da1c988bf673
saks <prohapro@gmail.com>
2022-06-09 14:37:14 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2); // определитель языка
+if ($lang == "ru"){
+header("Location: http://jointprojects.tk/ru.html"); // если ты русский то ты русский
+}else{
+    header("Location: http://jointprojects.tk/en.html"); // если ты не русский то ты англичанин
+}
+die(); // умер
+?>
